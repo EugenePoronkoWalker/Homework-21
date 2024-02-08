728ae2c3f8b3c2cf908bc61e134cc0c0d17a9df1
Eugene Poronko <eugenewalkerporonko@gmail.com>
2024-02-08 15:19:41 +0200

    Added new files for my homework 21

---
+++ helpers/data/user.py
@@ -0,0 +1,3 @@
+email = 'kharkiv1@gmail.com'
+nick = 'kha58461'
+password = 'KHA1234rkiv'
---
+++ homework21.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from pages import base
+import helpers.data.user as user_data
+import time
+# Або так, або так
+# from pages.base import HomePage as h
+# from pages.base import RegistrationPage as r
+
+driver = webdriver.Chrome()
+driver.set_window_position(2000, 600)
+driver.maximize_window()
+url = 'https://dumskaya.net/'
+driver.get(url)
+home = base.HomePage(driver)
+reg = base.RegistrationPage(driver)
+top = base.TopPage(driver)
+# home.open_articles_page()
+
+# Розібратись чого не працює
+# home.open_home_page()
+home.user_menu()
+home.registration_user()
+# reg.registration(user_data.email,user_data.nick,user_data.password)
+home.open_top_page()
+print(top.first_line())
